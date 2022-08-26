2b629e7fae236062145379891f2cfbb64f4652cb
AlexaaDb <corebuild-macpro@corebuild-macpros-MacBook-Pro.local>
2022-08-26 16:22:23 +0300

    feat(FO-6): add splash screen

--- src/Pages/SplashPage.tsx
+++ src/Pages/SplashPage.tsx
@@ -1,16 +1,49 @@
-import React, { FunctionComponent } from 'react';
-import { Button, View } from 'react-native';
-import { signUp } from '../Services/FirebaseService';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import React, { FunctionComponent, useEffect } from 'react';
+import { Image, StyleSheet, useWindowDimensions, View } from 'react-native';
+import { RootStackParamList } from '../App';
+import { checkIdToken } from '../Services/FirebaseService';
 
 const SplashPage: FunctionComponent = () => {
+  const { width } = useWindowDimensions();
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const isValidToken = await checkIdToken();
+        if (isValidToken) {
+          navigation.navigate('SignInPage');
+        } else {
+          navigation.navigate('SignUpPage');
+        }
+      } catch (error) {
+        console.warn('[SPLASH]', error);
+      }
+    };
+    checkUser();
+  }, []);
+
   return (
-    <View>
-      <Button
-        title='signin'
-        onPress={() => signUp('Jane Doe', 'jane.doe2@example.com', 'Test123!')}
+    <View style={styles.container}>
+      <Image
+        source={require('../../assets/images/grocery-sgi.png')}
+        style={{ ...styles.image, width }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F5F5F5',
+    justifyContent: 'center',
+    flex: 1
+  },
+  image: {
+    height: 330
+  }
+});
 
 export default SplashPage;
